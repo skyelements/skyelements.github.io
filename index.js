d75fa05bcdb9eef13bfbe96d285d4cc919bc1241
Java <yewshan.ooi@gmail.com>
2024-06-03 19:22:48 +0800

    feat: added Material Web dialog box for API keys

--- index.js
+++ index.js
@@ -35,40 +35,70 @@ async function getDogFact() {
 
 
 // Add, View, Remove & Clear API keys
-async function addAPIKey() {
-    let apiKey = document.getElementById('APIKeyInput').value;
+async function manageAPIKeys(param) {
+    const dialog = document.querySelector('md-dialog');
+        const title = dialog.querySelector('.dialog-title');
+        const description = dialog.querySelector('.dialog-description');
+        const button = dialog.querySelector('.dialog-button');
 
-    if (apiKey === '') {
-        alert('Error: Please enter an API key.')
-        return;
+        button.innerText = "OK";
+
+    switch(param) {
+        case 'add':
+            let apiKey = document.getElementById('APIKeyInput').value;
+
+            if (apiKey === '') {
+                title.innerText = "Error";
+                description.innerText = "Please enter an API key";
+
+                break;
+            }
+        
+            localStorage.setItem("GOOGLE_API_KEY", apiKey);
+
+            title.innerText = "";
+            description.innerText = "You have saved your API key";
+
+            break;
+
+        case 'view':
+            try {
+                const API_KEY = localStorage.getItem("GOOGLE_API_KEY");
+
+                if (API_KEY === null) {
+                    title.innerText = "Error";
+                    description.innerText = "You do not have an API key saved";
+
+                    break;
+                }
+
+                title.innerText = "Your API key is";
+                description.innerText = `${API_KEY}`;
+                // [To-Do] Add a Copy button for users to copy their API key
+            }
+            catch {
+                title.innerText = "Error";
+                description.innerText = "There was an error getting your API key";
+            }
+
+            break;
+
+        case 'removeGoogle':
+            localStorage.removeItem("GOOGLE_API_KEY");
+
+            title.innerText = "";
+            description.innerText = "You have removed your API key";
+
+            break;
+
+        case 'remove':
+            localStorage.clear();
+
+            title.innerText = "";
+            description.innerText = "You have removed all existing API keys";
+
+            break;
     }
 
-    localStorage.setItem("GOOGLE_API_KEY", apiKey);
-    alert('You have saved your API key!');
-    
-    window.location.reload();
-};
-
-async function viewAPIKey() {
-    try {
-        const API_KEY = localStorage.getItem("GOOGLE_API_KEY");
-        alert(`Your API key is: ${API_KEY}`);
-    }
-    catch {
-        alert('Error: There was an error getting your API key.')
-    }
-};
-
-async function removeAPIKey() {
-    localStorage.removeItem("GOOGLE_API_KEY");
-    alert('You have removed your API key!');
-
-    window.location.reload();
-};
-
-async function removeAllAPIKeys() {
-    localStorage.clear();
-    alert('You have removed all existing API keys!');
-
-    window.location.reload();
-};
+    dialog.setAttribute('open', '');
+}
