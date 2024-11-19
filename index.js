02771c3283c5024aa3c11d7974c7ef38394cfec1
Java <yewshan.ooi@gmail.com>
2024-11-19 14:02:57 +0800

    feat: added popup for copying Sodium clone link

--- index.js
+++ index.js
@@ -139,8 +139,18 @@ async function manageAPIKeys(param) {
 }
 
 // Copy Sodium GitHub clone command to clipboard
-function copySodiumClone() {
-    return navigator.clipboard.writeText("git clone https://github.com/yewshanooi/sodium.git");
+function sodiumCloneLink() {
+    const dialog = document.querySelector('md-dialog');
+
+    const title = dialog.querySelector('.dialog-title');
+    const description = dialog.querySelector('.dialog-description');
+    const button = dialog.querySelector('.dialog-button');
+
+        title.innerText = "Link";
+        description.innerText = "Copied to clipboard";
+        button.innerText = "Close";
+
+    return dialog.setAttribute('open', '').then(navigator.clipboard.writeText("git clone https://github.com/yewshanooi/sodium.git"));
 }
 
 // Sign in to SkyElements
