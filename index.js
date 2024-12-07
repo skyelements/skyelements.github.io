ec0fef766cf5b4d0ea4c9823a6cbb9092e5ae088
Java <yewshan.ooi@gmail.com>
2024-12-07 16:42:35 +0800

    fix: added a promise for navigator.clipboard.writeText method

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ function sodiumCloneLink() {
         description.innerText = "Copied to clipboard";
         button.innerText = "Close";
 
-    return dialog.setAttribute('open', '').then(navigator.clipboard.writeText("git clone https://github.com/yewshanooi/sodium.git"));
+    return navigator.clipboard.writeText("git clone https://github.com/yewshanooi/sodium.git").then(() => { dialog.setAttribute('open', '') });
 }
 
 // Sign in to SkyElements
